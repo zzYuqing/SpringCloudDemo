0cf95947368de9ce218759441fbc7adf24a4d063
zzYuqing <948160019@qq.com>
2024-04-04 16:51:34 +0800

    两个demo服务order和user，并通过RestTemplate实现了Order服务调用User

---
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -0,0 +1,31 @@
+package cn.itcast.order.web;
+
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import cn.itcast.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+   @Autowired
+   private OrderService orderService;
+   @Autowired
+   private RestTemplate restTemplate;
+
+    @GetMapping("{orderId}")
+    public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+        // 根据id查询订单并返回
+        Order order=orderService.queryOrderById(orderId);
+        String url="http://localhost:8081/user/"+order.getUserId();
+        order.setUser(restTemplate.getForObject(url, User.class));
+
+        return order;
+    }
+}
